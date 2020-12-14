69fa559b64aa585e21ec6645cbbd1fa9661eb02b
Bahtiyar <bahtiyarmceylan@gmail.com>
2020-12-14 14:36:56 -0500

    track spoitfy auth vars in an object

--- js/page_part1.js
+++ js/page_part1.js
@@ -4,23 +4,25 @@ $(function() {
 });
 
 // create spotify object
-var spotify_code;
-var spotify_access_token;
-var spotify_refresh_token;
-var spotify_expires_in;
-var spotify_is_expired;
-var spotify_timer;
+var spotifyAuth = {code: "", accessToken: "", refreshToken: "", expiresIn: 0, isExpired: true};
+var spotifyAuthTimer;
+// var spoitfy = new Object();
+// var spotify_code;
+// var spotify_access_token;
+// var spotify_refresh_token;
+// var spotify_expires_in;
+// var spotify_is_expired;
 
 function check_params() {
 	const queryString = window.location.search;
 	const urlParams = new URLSearchParams(queryString);
 	if(urlParams.has("code"));
-		spotify_code = urlParams.get("code");
+		spotifyAuth.code = urlParams.get("code");
 
 }
 
 function btn_temp() {
-	console.log(spotify_access_token);
+	console.log(spotifyAuth.accessToken);
 }
 
 function btn_login1() {
@@ -29,29 +31,31 @@ function btn_login1() {
 
 function btn_login2() {
 	$.get("./requests.php",
-		{ aspm_spotify_access: "1", code : spotify_code },
+		{ aspm_spotify_access: "1", code : spotifyAuth.code },
 		function(data, status) {
-			spotify_access_token = data["access_token"];
-			spotify_refresh_token = data["refresh_token"];
-			spotify_expires_in = data["expires_in"];
-			spotify_is_expired = false;
+			spotifyAuth.accessToken = data["access_token"];
+			spotifyAuth.refreshToken = data["refresh_token"];
+			spotifyAuth.expiresIn = data["expires_in"];
+			spotifyAuth.expiresIn = false;
 			start_spotify_expiration_countdown();
 		}, "json");
 }
 
 function btn_login3() {
 	$.get("./requests.php",
-		{ aspm_spotify_refresh: "1", refresh_token : spotify_refresh_token },
+		{ aspm_spotify_refresh: "1", refresh_token : spotifyAuth.refreshToken },
 		function(data, status) {
-			spotify_access_token = data["access_token"];
-			spotify_expires_in = data["expires_in"];
-			spotify_is_expired = false;
+			spotifyAuth.accessToken = data["access_token"];
+			spotifyAuth.refreshToken = data["expires_in"];
+			spotifyAuth.expiresIn = data["expires_in"];
+			spotifyAuth.expiresIn = false;
+			start_spotify_expiration_countdown();
 		}, "json");
 }
 
 function start_spotify_expiration_countdown() {
-	clearTimeout(spotify_timer);
-	spotify_timer = setTimeout(function() {
-		spotify_is_expired = true;
-	}, spotify_expires_in);
+	clearTimeout(spotifyAuthTimer);
+	spotifyAuthTimer = setTimeout(function() {
+		spotifyAuth.isExpired = true;
+	}, spotifyAuth.expiresIn);
 }
